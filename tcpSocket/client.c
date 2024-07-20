579ed5974e460bacc4a2c20e549d160da16321e2
EmreErcanBayer <eercan.bayer@gmail.com>
2024-07-20 22:16:16 +0300

    fix: main fixed

--- tcpSocket/client.c
+++ tcpSocket/client.c
@@ -1,13 +1,13 @@
-#include <arpa/inet.h>
-#include <errno.h>
-#include <netinet/tcp.h>
-#include <pthread.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <sys/socket.h>
-#include <unistd.h>
-#define THREAD_LIMIT 10
+# include <stdio.h>
+# include <stdlib.h>
+#include <stdnoreturn.h>
+# include <string.h>
+# include <sys/socket.h>
+# include <unistd.h>
+# include <arpa/inet.h>
+# include <netinet/tcp.h>
+# include <pthread.h>
+# define THREAD_LIMIT 10
 
 int iaIndexes[THREAD_LIMIT];
 
@@ -29,8 +29,7 @@ int connectSocket() {
     return -1;
   }
 
-  int connectionStatus =
-      connect(socketfd, (struct sockaddr *)&address, sizeof(address));
+  int connectionStatus = connect(socketfd, (struct sockaddr*)&address, sizeof(address));
   if (connectionStatus < 0) {
     perror("connection not established!\n");
     return -1;
@@ -38,42 +37,41 @@ int connectSocket() {
   return socketfd;
 }
 
-void *sendMessageTask(void *args) {
-  int iSocketIndex = *((int *)args);
+void* sendMessageTask(void* args) {
+  int iSocketIndex = *((int*)args);
   int socketfd = connectSocket();
-  char szMessage[1024] = {0};
-  for (int i = 0; i < 10; i++) {
-    sprintf(szMessage, "Hello, this is message %d from Client %d!\n", i,
-            iSocketIndex);
-    int if_send = send(socketfd, szMessage, strlen(szMessage), 0);
-    if (if_send == -1) {
-      perror("err send");
-      exit(-1);
+  char szMessage[100] = {0};
+  for (int i = 0; i < 2000; i++) {
+    sprintf(szMessage, "Hello, this is message %d from Client %d!\n", i, iSocketIndex);
+    int iRet=send(socketfd, szMessage, strlen(szMessage), 0);
+    if (iRet<0) {
+      perror("ERR");
+      close(socketfd);
+      exit(1);
     }
     printf("message sent!\n");
   }
-  shutdown(socketfd, SHUT_RDWR);
-  close(socketfd);
 
+  close(socketfd);
   return NULL;
 }
 
 int main() {
   pthread_t tasks[THREAD_LIMIT] = {0};
-
+  
   printf("client sockets starting...\n");
   for (int i = 0; i < THREAD_LIMIT; i++) {
     iaIndexes[i] = i + 1;
-    if (pthread_create(&tasks[i], NULL, &sendMessageTask,
-                       (void *)(&iaIndexes[i])) < 0) {
+    if (pthread_create(&tasks[i], NULL, &sendMessageTask, (void*)(&iaIndexes[i])) < 0) {
       perror("thread cannot created!\n");
       return -1;
     }
   }
 
-  for (int i = 0; i <1; i++) {
+  for (int i = 0; i < THREAD_LIMIT; i++) {
     pthread_join(tasks[i], NULL);
   }
+
 
   printf("All messages sent!\n");
 
--- tcpSocket/main.c
+++ tcpSocket/main.c
@@ -5,53 +5,67 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <strings.h>
 #include <sys/socket.h>
 #include <sys/types.h>
 #include <time.h>
 #include <unistd.h>
 
 #define QUEUE_LENGTH 10
-#define CLIENT_LIMIT 10000
-pthread_t tasks[CLIENT_LIMIT] = {0};
+#define CLIENT_LIMIT 100
+pthread_t tasks[CLIENT_LIMIT];
 int g_TaskVariable = 0;
 pthread_mutex_t mutexLock;
-
 
 void *ClientTask(void *clientSocketfd) {
 
   int client_socket = *(int *)(clientSocketfd);
   char szRecvArr[1025] = {0};
   int message_count = 0;
+
   while (1) {
 
-    int iMessageSize = recv(client_socket, szRecvArr, 1024,0);//setsockopt()
-    //sleep(10);
-    printf("iMessageSize %d\n", iMessageSize);
+    int iMessageSize = recv(client_socket, szRecvArr, 1024, 0); // setsockopt()
+    // sleep(10);
 
-    if (iMessageSize == 0 || iMessageSize == -1) {
+    if (iMessageSize == 0) {
 
       perror("recv Sock err.Terminating Thread \n");
       shutdown(client_socket, SHUT_RDWR);
 
       close(client_socket);
-      free(clientSocketfd);
-
-      break; // we will do this
-
-    } else if (iMessageSize) {
-
+      break;
+    } 
+    
+    else if (iMessageSize> 0 ) {
+  
       szRecvArr[iMessageSize] = '\0';
-      printf("%s", szRecvArr);
+
+      int count=0;
+      int iMessageCursor=0;
+
+      while (szRecvArr[iMessageCursor]!=0) {
+       
+       if (szRecvArr[iMessageCursor] == '\n') {
+        count++;
+       }
+        iMessageCursor++;
+      }
+      printf(" message_count:%d\n",count);
+      printf("%s end \n", szRecvArr);
 
       pthread_mutex_lock(&mutexLock);
-      
-      g_TaskVariable++;
-      printf("%d increased\n", g_TaskVariable);
-      
+
+      g_TaskVariable+=count;
+  
+      printf("Task VAR:%d\n", g_TaskVariable);
+
       pthread_mutex_unlock(&mutexLock);
     }
+
   }
 
+  free(clientSocketfd);
   return NULL;
 }
 
@@ -64,10 +78,22 @@ void main() {
   TCPServerAdd.sin_addr.s_addr = inet_addr("127.0.0.1");
   TCPServerAdd.sin_port = htons(5000);
   unsigned int addressSize = sizeof(TCPServerAdd);
-
+  int iSetSockOptions = 1;
   int iaClientSock[QUEUE_LENGTH] = {0};
   int *iClientSock;
   iServerSock = socket(AF_INET, SOCK_STREAM, 0);
+
+  if (setsockopt(iServerSock, SOL_SOCKET, SO_REUSEADDR, &iSetSockOptions,
+                 sizeof(iSetSockOptions)) < 0) {
+    perror("Something went wrong during port attach");
+    exit(EXIT_FAILURE);
+  }
+
+  if (setsockopt(iServerSock, SOL_SOCKET, SO_REUSEPORT, &iSetSockOptions,
+                 sizeof(iSetSockOptions)) < 0) {
+    perror("Something went wrong during port attach");
+    exit(EXIT_FAILURE);
+  }
 
   if (pthread_mutex_init(&mutexLock, 0) != 0) {
     perror("err mutex init");
@@ -92,6 +118,10 @@ void main() {
   while (1) {
 
     iClientSock = (int *)malloc(sizeof(int));
+    if (iClientSock == NULL) {
+      perror("malloc err\n");
+      exit(EXIT_FAILURE);
+    }
 
     (*iClientSock) = accept(iServerSock, (struct sockaddr *)&TCPServerAdd,
                             (socklen_t *)&addressSize);
@@ -101,9 +131,8 @@ void main() {
       perror("thread create err");
       exit(1);
     }
-    index++;
   }
-
+  // printf("glob var:%d\n",g_TaskVariable);
   shutdown(iServerSock, SHUT_RDWR);
   close(iServerSock);
   pthread_mutex_destroy(&mutexLock);
